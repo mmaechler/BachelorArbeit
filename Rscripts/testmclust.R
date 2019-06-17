952551abbc4c2e2a8f0f3d1e3c2512fe02bc1c40
xXx-faegbag-xXx <nicolas.trutmann@gmx.ch>
2019-06-17 13:50:07 +0200

    ""

--- Rscripts/testmclust.R
+++ Rscripts/testmclust.R
@@ -1,9 +1,9 @@
-### test script to learn mclust package
-## version 5.4.3
+### test script to learn mclust and mixtools package
 
 # data() lists all available example data sets
 
-library(mclust)
+library(mclust) # version 5.4.3
+library(mixtools) #version 1.1.0
 
 data(diabetes)
 
@@ -16,3 +16,31 @@ table(class)
 X <- diabetes[,-1]
 
 clPairs(X, class)
+
+
+# normalmixEM from mixtools package
+
+library(nor1mix)
+
+x9 <- rnorMix(MW.nm9, n=500)
+mixem <- normalmixEM(x9)
+mixem$mu
+# [1] -1.1039705  0.9953695
+mixem$sigma
+# [1] 0.6942218 0.7528372
+#only found 2 clusters
+plot(mixem$all.loglik, type="l") #stagnated after 50 iterations
+
+x9 <- rnorMix(MW.nm9, n=5000) #give more data
+mixem$mu
+# [1]  1.035134 -1.120661
+mixem$sigma
+# [1] 0.7204488 0.6556280
+#compare to actual:
+MW.nm9
+# 'Normal Mixture' object 	 ``#9 Trimodal'' 
+#        mu sigma    w
+# [1,] -1.2  0.60 0.45
+# [2,]  1.2  0.60 0.45
+# [3,]  0.0  0.25 0.10
+plot(mixem$all.loglik, type="l") #similar results
