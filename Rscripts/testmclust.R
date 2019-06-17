d8e0160fe6fd78403e85b73737b6c2c88f5c2695
xXx-faegbag-xXx <nicolas.trutmann@gmx.ch>
2019-06-17 22:16:31 +0200

    "some more R tests"

--- Rscripts/testmclust.R
+++ Rscripts/testmclust.R
@@ -17,6 +17,40 @@ X <- diabetes[,-1]
 
 clPairs(X, class)
 
+x9 <- rnorMix(MW.nm9, n=5000)
+
+y9 <- Mclust(x9)
+summary(y9, par=TRUE)
+# ---------------------------------------------------- 
+# Gaussian finite mixture model fitted by EM algorithm 
+# ---------------------------------------------------- 
+# Mclust E (univariate, equal variance) model with 3 components: 
+#  log-likelihood    n df      BIC       ICL
+#       -7846.796 5000  6 -15744.7 -17150.79
+# Clustering table:
+#    1    2    3 
+# 1978 1019 2003 
+# Mixing probabilities:
+#         1         2         3 
+# 0.3850980 0.2224116 0.3924904 
+# Means:
+#           1           2           3 
+# -1.34250220 -0.05709185  1.31779230 
+# Variances:
+#         1         2         3 
+# 0.2874259 0.2874259 0.2874259 
+
+#somewhat off, how to controll iterations
+
+
+	z9 <- em(modelName="V", x9,   ) # needs init param
+
+emctl <- emControl(itmax=50000)
+y9 <- Mclust(x9, control= emctl)
+summary(y9, par=TRUE) # still not accurate at itmax 50000. 
+# maybe stuck at loc. opt.
+
+
 
 # normalmixEM from mixtools package
 
@@ -44,3 +78,9 @@ MW.nm9
 # [2,]  1.2  0.60 0.45
 # [3,]  0.0  0.25 0.10
 plot(mixem$all.loglik, type="l") #similar results
+
+
+
+## comparing packages
+
+
