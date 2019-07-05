f000f9f3b4b51db829797d269b57a78109c73d0d
xXx-faegbag-xXx <nicolas.trutmann@gmx.ch>
2019-07-05 15:34:20 +0200

    ""

---
+++ Rscripts/Cholesky.R
@@ -0,0 +1,52 @@
+### LDL Cholesky decomposition, not particularly efficient, but works for now
+
+
+ldl <- function(mat){
+
+	stopifnot( is.matrix(mat)&&is.numeric(mat)&&nrow(mat)==ncol(mat) )
+
+	n <- ncol(mat)
+	Diag <- rep(0,n)
+	L <- matrix(0,n,n)
+	mat.copy <- mat
+
+	for (i in 1:n){
+		Diag[i] <- mat[i,i]
+		L[,i] <- mat[,i]/Diag[i]
+		mat <- mat - Diag[i]*tcrossprod(L[,i],L[,i])
+		mat[,i] <- rep(0,n)
+		mat[i,] <- rep(0,n)
+	}
+
+	list(L=L, Diag=Diag)
+}
+
+
+FF <- cbind( c(4.2,12,-16), c(12,37.07,-43), c(-16,-43,98.43) )
+FF
+ldl(FF)
+# $L
+#      [,1] [,2] [,3]
+# [1,]    1    0    0
+# [2,]    3    1    0
+# [3,]   -4    5    1
+# 
+# $Diag
+# [1] 4 1 9
+# 
+
+system.time(  {for (i in 1:10000){ldl(FF)}}  )
+#    user  system elapsed 
+#   0.244   0.000   0.246 
+
+# with isSymmetric
+system.time(  {for (i in 1:10000){ldl(FF)}}  )
+#    user  system elapsed 
+#   2.236   0.000   2.235 
+
+
+# system.time(  {for (i in 1:1000000){ldl(FF)}}  )
+#    user  system elapsed 
+#  24.404   0.008  24.434 
+
+
--- Rscripts/param.R
+++ Rscripts/param.R
@@ -1,6 +1,7 @@
 #### functions handling parameter manipulation. par2nM nM2par etc
 
 
+source(file="Cholesky.R")
 
 ## a way to make upper/lower triangular matrix into vector without lower/upper half
 
@@ -13,8 +14,20 @@
 
 ## works
 
-udtv <- function(mat){
-	upper.tri(chol(mat), diag=TRUE)
+
+#wrapper function for lower.tri to vector
+
+ld. <- function(mat){
+	x <- mat[lower.tri(mat, diag=FALSE)]
+	x
+}
+
+dl. <- function(d,x,p){
+	mat <- matrix(0,p,p)
+	mat[lower.tri(mat,diag=FALSE)==TRUE] <- x
+	mat.temp <- mat + diag(rep(1,p))  
+	mat <- mat.temp %*% diag(d) %*% t(mat.temp)
+	mat
 }
 
 
@@ -61,31 +74,57 @@ nMm2par <- function(obj,
 
 			  "VII" = sig[1,1,],
 
-			  "EEI" = {D <- diag(sig[,,1])
-			  	   alpha <- (prod(D)^(1/p))
+			  "EEI" = {D. <- diag(sig[,,1])
+			  	   alpha <- (prod(D.)^(1/p))
 			  	   c(alpha, D)
-			  	  }
+			  	  },
 
 			  "VEI" = {alpha <- apply(sig, 3, function(j) prod(diag(j))^(1/p) )
-			  	   D <- diag(sig[,,1])/alpha[1]
-			  	   c(alpha, D)
+			  	   D. <- diag(sig[,,1])/alpha[1]
+			  	   c(alpha, D.)
 				  },
 			  
 			  "EVI" = {alpha <- prod(diag(sig[,,1]))^(1/p)
-			  	   D <- apply(sig,3,diag) # need to divide by alpha
-			  	   c(alpha,D)
+			  	   D. <- apply(sig,3,diag)/alpha
+			  	   c(alpha,D.)
 			  	  },
 
-			  "VVI" = {alpha <- apply(sig, 3, function(j) det(j)^(1/p)) 
-			  	   D <- apply(), # need to divide by alpha
+			  "VVI" = {alpha <- apply(sig,3, function(j) det(j)^(1/p)) 
+			  	   D.temp <- apply(sig,3,diag)
+			 	   D. <- D.temp %*% diag(1/alpha) # this is fastest?? https://stackoverflow.com/questions/20596433/how-to-divide-each-row-of-a-matrix-by-elements-of-a-vector-in-r
+				  },
 
-			  "EEE" = udtv(sig[,,1]),
+			  "EEE" = {A.temp <- ldl(sig)
+			  	   L <- A.temp$L
+			  	   D.temp <- A.temp$Diag
+				   D. <- D.temp/(lambda <- prod(D.temp)^(1/p))
+				   c( alpha, D., ld.(L) )
+				  },
 
-			  "EVE" = ,
+			  "EVE" = {A.temp <- ldl(sig[,,1])
+			  	   D.temp <- A.temp$Diag
 
-			  "VEE" = ,
+			  	   L <- A.temp$L
+				   alpha <- prod(D.temp)^(1/p)
+				   D. <- c( apply(sig,3, function(j) ldl(j)$Diag/alpha) )
 
-			  "VVE" = ,
+				   c( alpha, D., ld.(L) )
+			  	  },
+
+			  "VEE" = {alpha <- apply(sig,3, function(j) prod(ldl(j)$Diag)^(1/p))
+			  	   L <- ldl(sig[,,1])$L
+			  	   D. <- ldl(sig[,,1])/alpha[1]
+
+				   c(alpha, D., ld.(L))
+				  },
+
+			  "VVE" = {alpha <- apply(sig,3, function(j) prod(ldl(j)$Diag)^(1/p))
+			  	   L <- ldl(sig[,,1]$L)
+			  	   D.temp <- apply(sig,3, function(j) ldl(j)$Diag)
+				   D. <- D.temp %*% diag(1/alpha)
+
+				   c(alpha, D., ld.(L))
+				  },
 
 			  "EEV" = ,
 
@@ -93,11 +132,11 @@ nMm2par <- function(obj,
 
 			  #"EVV" = ,same as VVV
 
-			  "VVV" = {S <- matrix(p*(p+1L),k)
-			  	for (i in 1:k){
-					S[,i] <- udtv(sig[,,i])
-				}
-			  },
+			  #"VVV" = {S <- matrix(p*(p+1L),k)
+#			  for (i in 1:k){
+#					S[,i] <- ldl(sig[,,i])$L
+#				}
+#			  },
 
 			  stop("invalid argument in 'model'")
 			  )
