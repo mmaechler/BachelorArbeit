bf514051aa49611fa1279619e5a82a7331d6d0ec
TrN <nicolas.trutmann@gmx.ch>
2019-08-19 01:35:12 +0200

    "some minor errors"

--- Rscripts/Auswertung-fit-nm.R
+++ Rscripts/Auswertung-fit-nm.R
@@ -6,16 +6,16 @@ rdat <- list.files("~/BachelorArbeit/norMmix/data", pattern="mw.*")
 for (i in rdat) {
     print(paste("work on data set:",i))
     x <- readRDS(paste0("~/BachelorArbeit/norMmix/data/", i))
-    ans1 <- fit.norMmix(x, k=1:3, models=1:2, trafo="clr1", ini="cla", maxiter=300)
+    ans1 <- fit.norMmix(x, k=1:3, models=1:2, trafo="clr1", ini="clara", maxiter=300)
     saveRDS(ans1, file=paste0("~/BScThesis/4MM/","clr1.cla.k17m110.",i))
 
-    ans2 <- fit.norMmix(x, k=1:7, models=1:10, trafo="logit", ini="cla", maxiter=300)
+    ans2 <- fit.norMmix(x, k=1:7, models=1:10, trafo="logit", ini="clara", maxiter=300)
     saveRDS(ans2, file=paste0("~/BScThesis/4MM/","logit.cla.k17m110.",i))
 
-    ans3 <- fit.norMmix(x, k=1:7, models=1:10, trafo="clr1", ini="mcl", maxiter=300)
+    ans3 <- fit.norMmix(x, k=1:7, models=1:10, trafo="clr1", ini="mclVVV", maxiter=300)
     saveRDS(ans3, file=paste0("~/BScThesis/4MM/","clr1.mcl.k17m110.",i))
 
-    ans2 <- fit.norMmix(x, k=1:7, models=1:10, trafo="logit", ini="mcl", maxiter=300)
+    ans2 <- fit.norMmix(x, k=1:7, models=1:10, trafo="logit", ini="mclVVV", maxiter=300)
     saveRDS(ans2, file=paste0("~/BScThesis/4MM/","logit.mcl.k17m110.",i))
 }
 
