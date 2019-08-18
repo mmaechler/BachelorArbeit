63799d71e27265fc27d897c3ebb347263180f9e4
TrN <nicolatr@student.ethz.ch>
2019-08-19 01:25:07 +0200

    merge issues

--- Rscripts/Auswertung-fit-nm.R
+++ Rscripts/Auswertung-fit-nm.R
@@ -1,23 +1,21 @@
-devtools::load_all("~/ethz/BA/norMmix")
+devtools::load_all("BachelorArbeit/norMmix")
 
-
-rdat <- list.files("~/ethz/BA/Rscripts", pattern="mw.*")
+rdat <- list.files("~/BachelorArbeit/norMmix/data", pattern="mw.*")
 
 
 for (i in rdat) {
     print(paste("work on data set:",i))
-    x <- readRDS(paste0("~/ethz/BA/Rscripts/", i))
+    x <- readRDS(paste0("~/BachelorArbeit/norMmix/data/", i))
     ans1 <- fit.norMmix(x, k=1:3, models=1:2, trafo="clr1", ini="cla", maxiter=300)
-    saveRDS(ans1, file=paste0("~/ethz/BA/Rscripts/","clr1.cla.k17m110.",i))
+    saveRDS(ans1, file=paste0("~/BScThesis/4MM/","clr1.cla.k17m110.",i))
 
     ans2 <- fit.norMmix(x, k=1:7, models=1:10, trafo="logit", ini="cla", maxiter=300)
-    saveRDS(ans2, file=paste0("~/ethz/BA/Rscripts/","logit.cla.k17m110.",i))
+    saveRDS(ans2, file=paste0("~/BScThesis/4MM/","logit.cla.k17m110.",i))
 
     ans3 <- fit.norMmix(x, k=1:7, models=1:10, trafo="clr1", ini="mcl", maxiter=300)
-    saveRDS(ans3, file=paste0("~/ethz/BA/Rscripts/","clr1.mcl.k17m110.",i))
+    saveRDS(ans3, file=paste0("~/BScThesis/4MM/","clr1.mcl.k17m110.",i))
 
     ans2 <- fit.norMmix(x, k=1:7, models=1:10, trafo="logit", ini="mcl", maxiter=300)
-    saveRDS(ans2, file=paste0("~/ethz/BA/Rscripts/","logit.mcl.k17m110.",i))
-
+    saveRDS(ans2, file=paste0("~/BScThesis/4MM/","logit.mcl.k17m110.",i))
 }
 
