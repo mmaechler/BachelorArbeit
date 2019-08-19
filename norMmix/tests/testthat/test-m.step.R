71386ac7004600cde92a4e0a5bcd30a6b4c0a768
TrN <nicolas.trutmann@gmx.ch>
2019-08-19 17:32:11 +0200

    "mstep tested and correct, given use of mclust mstepVVV"

---
+++ norMmix/tests/testthat/test-m.step.R
@@ -0,0 +1,53 @@
+context("test m.step correctness")
+
+
+test_that("test against mclust m.step", {
+
+    set.seed(2019); x <- matrix(runif(20), 10, 2)
+
+    clu <- cluster::clara(x,2)$clustering
+
+    library(mclust)
+
+    index <- matrix(0, 10,2)
+    index[cbind(1:10,clu)] <- 1
+
+    mc <- mclust::mstep("VVV", x, index)
+
+    nm <- mstep.nMm(x, index)
+
+
+    expect_equal(mc$parameters$pro, nm$weight)
+    expect_equal(c(mc$parameters$mean), c(nm$mu))
+    expect_equal(c(mc$parameters$variance$sigma), c(nm$Sigma))
+
+})
+
+
+test_that("test over MW* mcl against nMm", {
+
+    MWdat <- Filter(function(.) is.norMmix(get(., "package:norMmix")),
+                     ls("package:norMmix", pattern = "^MW[1-9]"))
+
+    for (i in MWdat) {
+        for(j in 3:5) {
+            nMm <- get(i, "package:norMmix")
+            p <- nMm$dim
+            set.seed(2014+j); x <- rnorMmix(200, nMm)
+
+            for (k in 3:7) {
+                clu <- cluster::clara(x,k)$clustering
+                index <- matrix(0, 200,k)
+                index[cbind(1:200,clu)] <- 1
+
+                mc <- mclust::mstep("VVV", x, index)
+                nm <- mstep.nMm(x, index)
+
+                expect_equal(mc$parameters$pro, nm$weight)
+                expect_equal(c(mc$parameters$mean), c(nm$mu))
+                expect_equal(c(mc$parameters$variance$sigma), c(nm$Sigma))
+            }
+        }
+    }
+})
+
